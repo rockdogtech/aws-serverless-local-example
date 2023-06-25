58298784b7c3fb4cf3365e01412ce48c66f9ecb0
John Costa <john.costa@gmail.com>
2023-06-25 10:15:16 -0400

    no pem configured

--- example/main.go
+++ example/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
-	"io"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/beatlabs/github-auth/app/inst"
 	"github.com/beatlabs/github-auth/key"
 	"github.com/google/go-github/v53/github"
+	"os"
 )
 
 var (
@@ -16,38 +17,79 @@ var (
 	DefaultHTTPGetAddress = "https://checkip.amazonaws.com"
 
 	// ErrNoIP No IP found in response
-	ErrNoIP = errors.New("No IP in HTTP response")
+	ErrNoIP = errors.New("no IP in HTTP response")
 
 	// ErrNon200Response non 200 status code in response
-	ErrNon200Response = errors.New("Non 200 Response found")
+	ErrNon200Response = errors.New("non 200 Response found")
+
+	ErrNoGithubPemConfigured = errors.New("no Github Pem Configured")
+
+	ErrNoGithubApplicationIdConfigured = errors.New("no Github Application Id Configured")
+
+	ErrNoGithubInstallationIdConfigured = errors.New("no Github Installation Id Configured")
+
+	ErrUnableToParsePem = errors.New("unable to parse pem")
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+type GithubProxyResponse struct {
+	StatusCode        int                 `json:"statusCode"`
+	Headers           map[string]string   `json:"headers"`
+	MultiValueHeaders map[string][]string `json:"multiValueHeaders"`
+	Body              string              `json:"body"`
+	IsBase64Encoded   bool                `json:"isBase64Encoded,omitempty"`
+}
+
+func handler(request events.APIGatewayProxyRequest) (GithubProxyResponse, error) {
 	//resp, err := http.Get(DefaultHTTPGetAddress)
 	//if err != nil {
 	//	return events.APIGatewayProxyResponse{}, err
 	//}
-	pem, err := key.Parse()
+	apiKey := os.Getenv("GITHUB_AUTH_PEM")
+	if apiKey == "" {
+		return GithubProxyResponse{}, ErrNoGithubPemConfigured
+	}
 
-	client := github.NewClient(nil)
+	installIDEnvValue := os.Getenv("GITHUB_AUTH_INST_ID")
+	if installIDEnvValue == "" {
+		return GithubProxyResponse{}, ErrNoGithubInstallationIdConfigured
+	}
+
+	appIDEnvValue := os.Getenv("GITHUB_AUTH_APP_ID")
+	if appIDEnvValue == "" {
+		return GithubProxyResponse{}, ErrNoGithubApplicationIdConfigured
+	}
+
+	pemBytes, err := key.Parse([]byte(apiKey))
+	if err != nil {
+		return GithubProxyResponse{}, ErrUnableToParsePem
+	}
+	githubAppInstallation, err := inst.NewConfig(appIDEnvValue, installIDEnvValue, pemBytes)
+	ctx := context.Background()
+
+	client := github.NewClient(githubAppInstallation.Client(ctx))
+	opt := &github.RepositoryListByOrgOptions{Type: "public"}
+	repos, resp, err := client.Repositories.ListByOrg(context.Background(), "github", opt)
 
 	if resp.StatusCode != 200 {
-		return events.APIGatewayProxyResponse{}, ErrNon200Response
+		return GithubProxyResponse{}, ErrNon200Response
 	}
 
-	ip, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
+	for repo := range repos {
+		fmt.Printf("repo %+v", repo)
 	}
 
-	if len(ip) == 0 {
-		return events.APIGatewayProxyResponse{}, ErrNoIP
-	}
-
-	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("Hello, %v", string(ip)),
+	return GithubProxyResponse{
 		StatusCode: 200,
 	}, nil
+
+	//ip, err := io.ReadAll(resp.Body)
+	//if err != nil {
+	//	return events.APIGatewayProxyResponse{}, err
+	//}
+	//
+	//if len(ip) == 0 {
+	//	return events.APIGatewayProxyResponse{}, ErrNoIP
+	//}
 }
 
 func main() {
